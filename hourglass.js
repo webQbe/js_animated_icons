efb011054965d89525e88e6b759d98a60b5a4b2f
webQbe <webcubesolutionslk@gmail.com>
2024-10-18 14:17:35 +0530

    Repeated Hourglass filling animation

--- hourglass.js
+++ hourglass.js
@@ -1,19 +1,35 @@
 // Hourglass Tip Animation
- let hourglass = document.getElementById('hourglass');
- hourglass.innerHTML = "&#xf251;";
 
- // change icon in 1 sec
- setTimeout(function(){
+function hourglassFill(){
 
-    hourglass.innerHTML = "&#xf252;";
+    let hourglass = document.getElementById('hourglass');
+    hourglass.innerHTML = "&#xf251;";
+   
+    // change icon in 1 sec
+    setTimeout(function(){
+   
+       hourglass.innerHTML = "&#xf252;";
+   
+    }, 1000)
+   
+     // change icon in 2 sec
+     setTimeout(function(){
+   
+       hourglass.innerHTML = "&#xf253;";
+   
+    }, 2000)
 
- }, 1000)
+}
 
-  // change icon in 2 sec
-  setTimeout(function(){
+// Run Animation
+hourglassFill();
 
-    hourglass.innerHTML = "&#xf253;";
+// Repeat Animation every 3sec
+setInterval(hourglassFill, 3000);
 
- }, 2000)
+
+
+
+
 
 
