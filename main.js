602a0ee24f04581a2805ebbb418a1b2d2f0004b2
webQbe <webcubesolutionslk@gmail.com>
2024-10-18 13:58:14 +0530

    Animated Battery icons

--- main.js
+++ main.js
@@ -5,6 +5,30 @@ function chargeBattery(){
     let battery = document.getElementById('battery');
     battery.innerHTML = "&#xf244;";
 
+    
+    // change icon in 1 sec
+    setTimeout(function(){
+        battery.innerHTML = "&#xf243;";
+    }, 1000);
+
+
+    // change icon in 2 sec
+    setTimeout(function(){
+        battery.innerHTML = "&#xf242;";
+    }, 2000);
+
+
+    // change icon in 3 sec
+    setTimeout(function(){
+        battery.innerHTML = "&#xf241;";
+    }, 3000);
+
+    // change icon in 4 sec
+    setTimeout(function(){
+        battery.innerHTML = "&#xf240;";
+    }, 4000);
+
 }
 
+// Run Animation
 chargeBattery();
