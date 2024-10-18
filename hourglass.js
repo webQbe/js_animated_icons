146d53b47316bf06fd6df783554630d0424217ec
webQbe <webcubesolutionslk@gmail.com>
2024-10-18 14:12:03 +0530

    Animated Hourglass icon

--- hourglass.js
+++ hourglass.js
@@ -2,4 +2,18 @@
  let hourglass = document.getElementById('hourglass');
  hourglass.innerHTML = "&#xf251;";
 
- 
+ // change icon in 1 sec
+ setTimeout(function(){
+
+    hourglass.innerHTML = "&#xf252;";
+
+ }, 1000)
+
+  // change icon in 2 sec
+  setTimeout(function(){
+
+    hourglass.innerHTML = "&#xf253;";
+
+ }, 2000)
+
+
